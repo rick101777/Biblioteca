50b590568078d25e209f6f7cfbf2e951e6619ba9
rick101777 <rick101777@yahoo.com>
2019-05-26 16:43:08 -0500

    Changed Main User interaction. Options are conducted through static methods. Some Library Test Cases have been written to confirm Checkout, Search, and Return work properly.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,58 +1,71 @@
 package com.twu.biblioteca;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BibliotecaApp {
 
 
-    public static void main(String[] args) {
+    public static int mainMenu(Scanner scan){
+        int choice = -1;
+        do {
+            System.out.println("Main Menu:\n\t 1. List of books \n\t 2. Exit Application");
+            System.out.println("Enter your choice: ");
+            choice = scan.nextInt();
+            if (choice <= 0 || choice >= 3) { System.out.println("That is an invalid option. Try Again...");}
+        }while (choice <= 0 || choice >= 3);
 
+
+        return choice;
+    }
+
+    public static int bookMenuOption(Scanner scan){
+        int choice = -1;
+        do {
+            System.out.println("Book Action Menu:\n\t 1. Checkout a book \n\t 2. Return a Book \n\t 3. Return to Main Menu \n\t 4. Exit Application");
+            System.out.println("Enter your choice: ");
+            choice = scan.nextInt();
+        }while(choice <= 0 || choice >= 5);
+        return choice;
+    }
+
+    public static String getTitle(Scanner scan){
+        String title = "";
+        do {
+
+        }while (title != "");
+
+        return title;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         Library library = new Library();
         library.add(new Book("Le Petit Prince", "Antoine de Saint-Exupery", 1943));
         library.add(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997));
         library.add(new Book("The Hobbit", "J.R.R. Tolkien", 1937));
         library.add(new Book("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 1950));
 
-        Scanner scan = new Scanner(System.in);
-
-
         System.out.println("Welcome to Biblioteca. Your one stop-shop for great book titles in Bangalore!");
-        int choice = 2;
-        while(true){
-            System.out.println("Main Menu:\n\t 1. List of books \n\t 2. Exit Application");
-            System.out.println("Enter your choice: ");
 
-            try {
-                choice = scan.nextInt();
-            }catch(InputMismatchException ime){ System.out.println("That is not a valid option."); }
-
-            switch(choice){
+        boolean shouldRun = true;
+        while (shouldRun){
+            int MainActionChoice = mainMenu(scan);
+            switch (MainActionChoice){
                 case 1:
-                    System.out.println("");
                     System.out.println(library.toString());
-                    boolean Continue = true;
-                    while(Continue){
-                        System.out.println("Book Action Menu:\n\t 1. Checkout a book \n\t 2. Return a book \n\t 3. Return to Main Menu \n\t 4. Exit Application");
-                        System.out.println("Enter choice: ");
-                        try{
-                            choice = scan.nextInt();
-                        }catch(InputMismatchException ime){ }
-                        String bookTitle;
-                        switch (choice){
+                    boolean BookActions = true;
+                    while (BookActions){
+                        int BookActionChoice = bookMenuOption(scan);
+                        switch (BookActionChoice){
                             case 1:
-                                System.out.println("Enter book title: ");
-                                bookTitle = scan.nextLine();
-                                library.Checkout(bookTitle);
+
                                 break;
                             case 2:
-
                                 break;
                             case 3:
-                                Continue = false;
                                 break;
                             case 4:
-                                System.exit(0);
                                 break;
                             default:
                                 break;
@@ -60,13 +73,15 @@ public class BibliotecaApp {
                     }
                     break;
                 case 2:
-                    System.out.println("Application will close....");
-                    System.exit(0);
+                    shouldRun = false;
                     break;
                 default:
                     break;
             }
         }
+
+    }
+
         // Display Options for user:
             // List Books
                 // Options:
@@ -74,9 +89,5 @@ public class BibliotecaApp {
                     // Return book
                     // Exit
             // Exit
-
-
-
-
-    }
 }
+
